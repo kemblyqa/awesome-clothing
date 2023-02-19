bb33d7a8d63b7526fc491af9684d9ea355a6c9a4
kembly.quiros <kembly.quiros@golabstech.com>
2023-02-19 15:29:29 -0600

    initial project and components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.scss";
+import categories from "./categories.json";
+import { Directory } from "./components/Directory";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Directory categories={categories} />
   );
-}
+};
 
 export default App;
---
+++ src/components/Directory/index.jsx
@@ -0,0 +1,12 @@
+import "./styles.scss";
+import { CategoryItem } from "../CategoryItem";
+
+export const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
