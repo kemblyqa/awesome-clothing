628743956bfcdabad65eb0b268281f85e57f986e
kembly.quiros <kemblyqa@gmail.com>
2023-02-26 19:27:44 -0600

    addig useReducer as example inside contexts

--- src/contexts/UserContext/index.jsx
+++ src/contexts/UserContext/index.jsx
@@ -1,8 +1,9 @@
-import React, { createContext, useEffect, useState, useReducer } from "react";
+import React, { createContext, useEffect, useReducer } from "react";
 import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
 } from "../../utils/firebase";
+import { createAction } from "../../utils/reducer";
 
 export const UserContext = createContext({
   currentUser: null,
@@ -32,7 +33,7 @@ export const UserProvider = ({ children }) => {
   const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
 
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   };
 
   const value = { currentUser, setCurrentUser };
